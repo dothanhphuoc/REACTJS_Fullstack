ae46c0beff1fea7916a7492d7cb6f2196b939666
Do Thanh Phuoc <dothanhphuoc0706@gmail.com>
2023-08-18 23:08:55 +0700

    Su Dung State va Cach Xu Lý Su Kien Event OnClick, OnChange voi ReactJs

--- src/containers/Auth/Login.js
+++ src/containers/Auth/Login.js
@@ -3,10 +3,39 @@ import { connect } from 'react-redux';
 import { push } from "connected-react-router";
 import * as actions from "../../store/actions";
 import './Login.scss';
+import { Alert } from 'bootstrap';
 
 class Login extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            username: '',
+            password: '',
+            isShowPassword: false,
+        }
+    }
+
+    handleOnChangeUsername = (e) => {
+        this.setState({
+            username: e.target.value
+        })
+    }
+
+    handleOnChangePassword = (e) => {
+        this.setState({
+            password: e.target.value
+        })
+    }
+
+    handleLogin = () => {
+        console.log('username: ', this.state.username, "Password: ", this.state.password);
+        console.log("All State", this.state)
+    }
+
+    handleShowHidePassword = () => {
+        this.setState({
+            isShowPassword: !this.state.isShowPassword
+        })
     }
 
     render() {
@@ -20,24 +49,37 @@ class Login extends Component {
                         <div className='col-12 form-group login-input'>
 
                             <label>Username: </label>
-                            <input
-                                type='text'
-                                className='form-control'
-                                placeholder='Enter Username ...'
-                            />
+
+                            <div className='box-input'>
+                                <input
+                                    type='text'
+                                    className='form-control'
+                                    placeholder='Enter Username ...'
+                                    defaultValue={this.state.username}
+                                    onChange={(e) => this.handleOnChangeUsername(e)}
+                                />
+                                <i class="fa-regular fa-user"></i>
+                            </div>
                         </div>
 
                         <div className='col-12 form-group login-input'>
                             <label>Password: </label>
-                            <input
-                                type='password'
-                                className='form-control'
-                                placeholder='Enter Password ...'
-                            />
+
+                            <div className='box-input'>
+                                <input
+                                    type={this.state.isShowPassword ? 'text' : 'password'}
+                                    className='form-control'
+                                    placeholder='Enter Password ...'
+                                    defaultValue={this.state.password}
+                                    onChange={(e) => this.handleOnChangePassword(e)}
+                                />
+
+                                <i className={this.state.isShowPassword ? 'fa-regular fa-eye-slash' : 'fa-regular fa-eye'} onClick={() => this.handleShowHidePassword()}></i>
+                            </div>
                         </div>
 
                         <div className='col-12 text-center'>
-                            <button className='btn'>Login</button>
+                            <button className='btn' onClick={() => this.handleLogin()}>Login</button>
                         </div>
 
                         <div className='col-12 mt-3'>
@@ -49,8 +91,8 @@ class Login extends Component {
                         </div>
 
                         <div className='col-12 text-center'>
-                            <i class="fa-brands fa-facebook icon-action icon-facebook"></i>
-                            <i class="fa-brands fa-google-plus icon-action icon-google"></i>
+                            <i className="fa-brands fa-facebook icon-action icon-facebook"></i>
+                            <i className="fa-brands fa-google-plus icon-action icon-google"></i>
                         </div>
                     </div>
                 </div>
