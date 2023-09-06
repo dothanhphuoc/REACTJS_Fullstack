907024d41632c90b988aa3d1eb7c3e8684fffbb0
Do Thanh Phuoc <dothanhphuoc0706@gmail.com>
2023-09-06 09:53:05 +0700

    Customize React-Slider-Carousel

--- src/containers/HomePage/Section/Specialty.js
+++ src/containers/HomePage/Section/Specialty.js
@@ -6,42 +6,62 @@ import Slider from 'react-slick';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
+//Import img
+import cot_song from "../../../assets/specialty/cot-song.jpg";
+
 class Specialty extends Component {
 
     render() {
-
         let settings = {
             dots: false,
             infinite: true,
             speed: 500,
             lazyLoad: 'ondemand',
             slidesToShow: 3,
-            slidesToScroll: 1
+            slidesToScroll: 1,
         };
 
         return (
             <div className='section-specialty'>
-                <div className='specialty-content'>
-                    <Slider {...settings}>
-                        <div className='img-customize'>
-                            <h3>1</h3>
-                        </div>
-                        <div className='img-customize'>
-                            <h3>2</h3>
-                        </div>
-                        <div className='img-customize'>
-                            <h3>3</h3>
-                        </div>
-                        <div className='img-customize'>
-                            <h3>4</h3>
-                        </div>
-                        <div className='img-customize'>
-                            <h3>5</h3>
-                        </div>
-                        <div className='img-customize'>
-                            <h3>6</h3>
-                        </div>
-                    </Slider>
+                <div className='specialty-container'>
+                    <div className='specialty-header'>
+                        <h2>Chuyên Khoa Phổ Biến</h2>
+                        <a href=''>Xem thêm</a>
+                    </div>
+                    <div className='specialty-content'>
+                        <Slider {...settings}>
+                            <div className='specialty-box-item'>
+                                <div className='background-img'>
+                                </div>
+                                <h4 className='title-name'>Cột sống</h4>
+                            </div>
+                            <div className='specialty-box-item'>
+                                <div className='background-img'>
+                                </div>
+                                <h4 className='title-name'>Cột sống</h4>
+                            </div>
+                            <div className='specialty-box-item'>
+                                <div className='background-img'>
+                                </div>
+                                <h4 className='title-name'>Cột sống</h4>
+                            </div>
+                            <div className='specialty-box-item'>
+                                <div className='background-img'>
+                                </div>
+                                <h4 className='title-name'>Cột sống</h4>
+                            </div>
+                            <div className='specialty-box-item'>
+                                <div className='background-img'>
+                                </div>
+                                <h4 className='title-name'>Cột sống</h4>
+                            </div>
+                            <div className='specialty-box-item'>
+                                <div className='background-img'>
+                                </div>
+                                <h4 className='title-name'>Cột sống</h4>
+                            </div>
+                        </Slider>
+                    </div>
                 </div>
             </div>
         );
